3a974912465a6424ec14a476c10a37ef1f47041b
pirnataly <piroznatal@gmail.com>
2023-04-17 12:43:16 +0300

    feat: add extanded-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if(typeof str!=='string'){
+    str=(String(str) || str.toString()) ;
+  }
+  if(typeof options.addition!=='string'){
+    options.addition=( String(options.addition)||options.addition.toString());
+
+  }
+  let result='';
+  function getStr() {
+    let additionSeparator=options.additionSeparator?options.additionSeparator:'|';
+    let optionsAddition = (options.addition==='undefined')?'':options.addition;
+    additionSeparator=optionsAddition?additionSeparator:'';
+    console.log(optionsAddition)
+    for(let i=0;i<(options.additionRepeatTimes||1);i++) {
+      str=`${str}${optionsAddition}${additionSeparator}`;
+
+    }
+    console.log(str.substr(0, str.length-(additionSeparator.length || 0)));
+    return str= str.substr(0, str.length-(additionSeparator.length || 0));
+  }
+  getStr();
+  let separator;
+  for(let j=0;j<(options.repeatTimes || 1);j++){
+    result=`${result}${str}${separator=(options.separator || '+')}`;
+  }
+  return result.substr(0, result.length-(separator.length || 0))
 }
+const objWithSpecificCoercion = {
+  [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
+};
+
 
 module.exports = {
   repeater
